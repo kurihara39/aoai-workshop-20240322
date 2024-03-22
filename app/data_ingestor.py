bc9f340a713ff52bd52ef870897eb4f0cead890b
39_ARIGATO <80567773+kurihara39@users.noreply.github.com>
2024-03-22 05:59:29 +0000

    data ingesterの更新

--- app/data_ingestor.py
+++ app/data_ingestor.py
@@ -1,3 +1,4 @@
+import logging
 import azure.functions as func
 from azure.cosmos import CosmosClient
 import os
@@ -12,8 +13,9 @@ cosmos_client = CosmosClient.from_connection_string(os.getenv("COSMOS_CONNECTION
 def upload_json(req: func.HttpRequest) -> func.HttpResponse:
     container = cosmos_client.get_database_client(os.getenv("COSMOS_DB_NAME")).get_container_client(os.getenv("COSMOS_CONTAINER_NAME"))
 
-    items = req.get_json()
-    for item in items:
+    body = req.get_json()
+    data = body["data"]
+    for item in data:
         container.upsert_item(item)
 
-    return func.HttpResponse(f"{len(items)} data uploaded.", status_code=200)
+    return func.HttpResponse(f"{len(data)} data uploaded.", status_code=200)
